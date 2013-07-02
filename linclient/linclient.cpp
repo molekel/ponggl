077be5d26ef744f82913a643c3b2b34f3e288e0a
U-ikar-PC\ikar <ikar@ikar-PC.(none)>
2013-07-02 22:16:06 +0200

    modified:   linclient/linclient.cpp 	modified:   linserver/linserver.cpp

--- linclient/linclient.cpp
+++ linclient/linclient.cpp
@@ -2,66 +2,175 @@
 #include <io.h>
 #include <winsock2.h>
 //pragma ws2_32.lib
-/*
-WinSock2.h ln 119
-typedef UINT_PTR        SOCKET;
-einstweilen würd ich mal die pointer gleichsetzen problem :vorzeichenlose darstellung.. das könnte schiefgehen
-*/
 typedef struct {int x;int y;} vektor;
+
 
 	char sendstr[30]="bla";
 
 
-void writetoserver(int filedes){
-vektor position;
-position.x=111;
-position.y=999;
-int nbytes;
-
-nbytes=write(filedes,&position,8);//sizeof(int*2)
-if (nbytes<0)
-	exit(1);
-}
-
 int main(){
+
     WSADATA wsaData;
+
     int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+
+    if (iResult != NO_ERROR)
+
+              printf("Client: Error at WSAStartup().\n");
+
+    else
+
+              printf("Client: WSAStartup() is OK.\n");
+
+ 
+
+    // Create a socket.
+
     SOCKET msocket;
+
     msocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	sockaddr_in clientService;
+
+ 
+
+    if (msocket == INVALID_SOCKET)
+
+    {
+
+        printf("Client: socket() - Error at socket(): %ld\n", WSAGetLastError());
+
+        WSACleanup();
+
+        return 0;
+
+    }
+
+   else
+
+       printf("Client: socket() is OK.\n");
+
+ 
+
+    // Connect to a server.
+
+    sockaddr_in clientService;
 
  
 
     clientService.sin_family = AF_INET;
-	clientService.sin_addr.s_addr = inet_addr("127.0.0.1");
-	clientService.sin_port = htons(55555);
+
+    // Just test using the localhost, you can try other IP address
+
+    clientService.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    clientService.sin_port = htons(55555);
 
  
 
-    connect(msocket, (SOCKADDR*)&clientService, sizeof(clientService));
+    if (connect(msocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR)
+
+    {
+
+        printf("Client: connect() - Failed to connect.\n");
+
+        WSACleanup();
+
+        return 0;
+
+    }
+
+    else
+
+    {
+
+       printf("Client: connect() is OK.\n");
+
+       printf("Client: Can start sending and receiving data...\n");
+
+    }
+
+ 
+
+    // Send and receive data.
 
     int bytesSent;
 
-    //int bytesRecv = SOCKET_ERROR;
+    int bytesRecv = SOCKET_ERROR;
 
     // Be careful with the array bound, provide some checking mechanism...
 
     char sendbuf[200] = "This is a test string from client";
 
-    //char recvbuf[200] = "";
+    char recvbuf[200] = "";
 
  
 
-    //       bytesRecv = recv(msocket, recvbuf, 200, 0);
+       // Receives some test string from server...
 
-      //        printf("Client: Received data is: \"%s\"\n", recvbuf);
+       while(bytesRecv == SOCKET_ERROR)
 
-       bytesSent = send(msocket, sendbuf, strlen(sendbuf), 0);
+       {
+		   vektor testcoord;
+testcoord.x=22;
+testcoord.y=33;
 
+		   static vektor* datapointer=&testcoord;
+           bytesRecv = recv(msocket,(char *)datapointer, sizeof(vektor), 0);
+		  printf("%i",testcoord.x);
+       printf("%i",testcoord.y);
+		  if (bytesRecv == 0 || bytesRecv == WSAECONNRESET)
+
+        {
+
+             printf("Client: Connection Closed.\n");
+
+            break;
+
+        }
+
+ 
+
+        if (bytesRecv < 0)
+
+            return 0;
+
+       else
+
+       {
+
+              printf("Client: recv() is OK.\n");
+
+              printf("Client: Received data is: \"%s\"\n", recvbuf);
+
+              printf("Client: Bytes received is: %ld.\n", bytesRecv);
+
+       }
+
+    }
+
+ 
+
+       // Sends some test data to server...bytesSent = send(msocket, sendbuf, strlen(sendbuf), 0);
+	   
+	   bytesSent = send(msocket,(char *)sendbuf, strlen(sendbuf), 0);
+
+       if(bytesSent == SOCKET_ERROR)
+
+              printf("Client: send() error %ld.\n", WSAGetLastError());
+
+       else
+
+       {
+
+              printf("Client: send() is OK - Bytes sent: %ld\n", bytesSent);
+
+              printf("Client: The test string sent: \"%s\"\n", sendbuf);
+
+       }
+
+ 
 
     WSACleanup();
 
     return 0;
-
 
 }
--- linserver/linserver.cpp
+++ linserver/linserver.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include <stdio.h>
 #include <io.h>
 #include <winsock2.h>
@@ -9,119 +11,171 @@
 typedef struct {int x;int y;} vektor;
 
 	char readstring[30]="";
-	
-int readfromclient (int filedes)
-{
-vektor position;
-int buffer;
-int *bufferp;
-bufferp=&buffer;
-int nbytes;
-nbytes=read(filedes,&position,8);
-if (nbytes <0)
-{
-	exit(1);
-}
-else if (nbytes ==0)//eof
-	return -1;
-else
-{
-int test=888888;
-printf("printingint %i",test);
-printf("received posx %i",position.x);
-printf("received posy %i",position.y);
-return 0;
-}
-}
 
-void main(){
+
+int main(){
 
 WORD wVersionRequested;
 WSADATA wsaData;
 int wsaerr;
 
+ 
+
+// Using MAKEWORD macro, Winsock version request 2.2
 wVersionRequested = MAKEWORD(2, 2);
 wsaerr = WSAStartup(wVersionRequested, &wsaData);
 
 SOCKET m_socket;
+
 m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+
+
 sockaddr_in service;
 
 
 service.sin_family = AF_INET;
+// "127.0.0.1" is the local IP address to which the socket will be bound.
 service.sin_addr.s_addr = inet_addr("127.0.0.1");
+// 55555 is the port number to which the socket will be bound.
 service.sin_port = htons(55555);
 
  
 
-bind(m_socket, (SOCKADDR*)&service, sizeof(service)) ;
-listen(m_socket, 1);
+// Call the bind function, passing the created socket and the sockaddr_in structure as parameters.
+// Check for general errors.
+
+if (bind(m_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) 
+{
+    printf("Server: bind() failed: %ld.\n", WSAGetLastError());
+    closesocket(m_socket);
+    return 0;
+}
+else 
+{
+    printf("Server: bind() is OK!\n");
+}
+
+// Call the listen function, passing the created socket and the maximum number of allowed
+// connections to accept as parameters. Check for general errors.
+
+if (listen(m_socket, 10) == SOCKET_ERROR)
+{
+	printf("Server: listen(): Error listening on socket %ld.\n", WSAGetLastError());
+}
+else
+{
+printf("Server: listen() is OK, I'm waiting for connections...\n");
+
+}
+
+ 
+
+// Create a temporary SOCKET object called AcceptSocket for accepting connections.
+/*schleife umbauen*/
 SOCKET AcceptSocket;
 
-//while (1)
-//{
-//AcceptSocket = SOCKET_ERROR;
-//while (AcceptSocket == SOCKET_ERROR)
-//{
-AcceptSocket = accept(m_socket, NULL, NULL); //akzeptiert alles ... 
-//}
+ 
+
+// Create a continuous loop that checks for connections requests. If a connection
+// request occurs, call the accept function to handle the request.
+
+printf("Server: Waiting for a client to connect...\n" );
+
+printf("***Hint: Server is ready...run your client program...***\n");
+
+// Do some verification...
+
+while (1)
+
+{
+AcceptSocket = SOCKET_ERROR;
+while (AcceptSocket == SOCKET_ERROR)
+{
+AcceptSocket = accept(m_socket, NULL, NULL);
+}
 
  
+
+// else, accept the connection...
+// When the client connection has been accepted, transfer control from the
+// temporary socket to the original socket and stop checking for new connections.
+
+printf("Server: Client Connected!\n");
+
 m_socket = AcceptSocket; 
 
-//break;
+break;
 
-//}
+}
 
  
 
-//int bytesSent;
-//int bytesRecv = SOCKET_ERROR;
-//char sendbuf[200] = "This string is a test data from server";
+int bytesSent;
+
+int bytesRecv = SOCKET_ERROR;
+
+char sendbuf[200] = "9123456789";
+
+// initialize to empty data...
 
 char recvbuf[200] = "";
-//bytesSent = send(m_socket, sendbuf, strlen(sendbuf), 0);
+
  
-//recv(m_socket, recvbuf, 200, 0);
-int socketpointer=0;
-UINT_PTR dort;
-dort=m_socket;
-//socketpointer=*dort;
-//printf("sockp%i",socketpointer);
 
-read(dort,&recvbuf,20);	
+// Send some test string to client...
+printf("Server: Sending some test data to client...\n");
+int sender=543210;
+//bytesSent = send(m_socket, &sender,sizeof(int), 0);
+	   vektor testcoord;
+testcoord.x=98765;
+testcoord.y=44444;
+static vektor* vektorpointer=&testcoord;
 
+bytesSent = send(m_socket,(char *)vektorpointer, sizeof(vektor), 0);
 
-printf("Server: Received data is: \"%s\"\n", recvbuf);
+//bytesSent = write(&m_socket,&sender,sizeof(int));
+ 
 
+if (bytesSent == SOCKET_ERROR)
+       printf("Server: send() error %ld.\n", WSAGetLastError());
+else
+{
+       printf("Server: send() is OK.\n");
+       printf("Server: Bytes Sent: %ld.\n", bytesSent);
+}
 
+ 
+
+// Receives some test string from client...and client
+// must send something lol...
+
+bytesRecv = recv(m_socket, recvbuf, 200, 0);
+
+ 
+
+if (bytesRecv == SOCKET_ERROR)
+
+       printf("Server: recv() error %ld.\n", WSAGetLastError());
+
+else
+
+{
+     printf("Server: recv() is OK.\n");
+
+       printf("Server: Received data is: \"%s\"\n", recvbuf);
+
+       printf("Server: Bytes received: %ld.\n", bytesRecv);
+
+}
+
+ 
 
 WSACleanup();
 
+return 0;
 
-
-
-
-/*
-while (1)
-{
-//select() linux zeug..bzw wer connected
-int connection;
-int size;
-connection=accept(serversocket,NULL,NULL);
-	if (connection <0)
-		exit(1);
-	else 
-//	if (readfromclient(i)<0)
-	{
-		recv(serversocket,readstring,30,0);
-		printf("read%s",readstring);
-		//	close(serversocket);
-		WSACleanup();
-		exit(0);
-	}
-	*/
-	}
+}
  
 
 
